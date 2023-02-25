9479b81f6d5ff52a4650d58755c4ee30bff38127
Mahaveer86619 <psatyam86619@gmail.com>
2023-02-25 15:52:58 +0530

    Splash screen added

---
+++ app/src/main/java/com/example/erp/Startup/Splash_screen.java
@@ -0,0 +1,28 @@
+package com.example.erp.Startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.erp.R;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(Splash_screen.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        },2000);
+    }
+}
