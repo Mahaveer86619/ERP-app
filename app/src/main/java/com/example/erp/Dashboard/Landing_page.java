d3c864b418b10e77aab896debb0df470c98195c7
Mahaveer86619 <psatyam86619@gmail.com>
2023-02-25 23:00:14 +0530

    checkpoint commit

--- app/src/main/java/com/example/erp/Dashboard/Landing_page.java
+++ app/src/main/java/com/example/erp/Dashboard/Landing_page.java
@@ -1,40 +1,29 @@
 package com.example.erp.Dashboard;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.ProgressBar;
+
 import android.widget.TextView;
 
-import com.example.erp.Adapters.FragmentAdapter;
-import com.example.erp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.erp.databinding.ActivityLandingPageBinding;
 
 public class Landing_page extends AppCompatActivity {
 
     ActivityLandingPageBinding binding;
-    ProgressBar progressBar;
 
     int progress_status_attendance = 0, progress_status_fees = 0;
     TextView attendance_percent, fees_percent;
 
     Handler handler = new Handler();
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLandingPageBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
-        progressBar = (ProgressBar) findViewById(R.id.progressBar_attandance);
-
-        binding.viewpager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
-        binding.tablayout.setupWithViewPager(binding.viewpager);
 
     }
 }
